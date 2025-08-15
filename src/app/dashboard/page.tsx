dac8dd70769f51cf94c70b0852c5c48639f1a9aa
MarcioAlex-x <marcioalex586@gmail.com>
2025-08-15 09:30:35 -0300

    Página de login e função de login

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function dasboard(){
+    return(
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,79 @@
+'use client'
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+
+export default function login(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = async () =>{
+
+        try {
+            const response = await fetch('http://localhost:3001/login', {
+                method: 'POST',
+                headers:{
+                    'Content-Type':'application/json',
+                },
+                body:JSON.stringify({email, password})
+            })
+            const data = await response.json()
+
+            if(!response.ok){
+                throw new Error(data.message || 'Falha ao fazer o login.')
+            }
+
+            console.log('Login bem sucedido: ', data)
+            
+            alert('Logado')
+
+        } catch (error) {
+            console.error('Erro no login: ',  error)
+            alert(error.message)
+        }
+
+            
+    }
+
+    return(
+        <div className="grid place-items-center h-screen">
+            <div className="max-w-md">
+                <Card className="text-center p-4">
+                    <div>
+                        <CardHeader className="text-2xl">Login</CardHeader>
+                        <CardDescription className="text-sky-900">Faça login para continuar!</CardDescription>
+                    </div>
+
+                    <div className="p-2">
+                        <Label className="mb-2">E-mail:</Label>
+                        <Input
+                            type="text"
+                            placeholder="exemplo@email.com"
+                            id="email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            />
+                    </div>
+
+                    <div className="p-2 mb-4">
+                        <Label className="mb-2">Senha:</Label>
+                        <Input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <Button
+                        type="button"
+                        className="bg-sky-600 hover:bg-sky-700 cursor-pointer"
+                        onClick={handleLogin}>
+                            Entrar
+                    </Button>
+                </Card>
+            </div>
+        </div>
+    )
+}
